ba202ac778c27d978b55e535e2ab442c9f5b4bfe
Gustavo Lacerda <gustavo4kps4@gmail.com>
2026-02-22 17:26:33 -0300

    Update Exemplo1.java

--- Metodos/Exemplo1.java
+++ Metodos/Exemplo1.java
@@ -1,1 +1,22 @@
+import javax.swing.JOptionPane;
 
+public class Exemplo1 {
+
+    static void dobro(int n) {
+        int resultado = n * 2;
+        JOptionPane.showMessageDialog(null,
+                "Dobro: " + resultado);
+    }
+
+    public static void main(String[] args) {
+
+        int a;
+
+        // Caixa de diálogo removida aqui
+
+        a = Integer.parseInt(
+                JOptionPane.showInputDialog("Digite um número:"));
+
+        dobro(a);
+    }
+}

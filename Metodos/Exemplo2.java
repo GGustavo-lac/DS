3f5a674f6fdec83c8b5ccdff4600678b4b937e06
Gustavo Lacerda <gustavo4kps4@gmail.com>
2026-02-22 17:27:00 -0300

    Comment out Exemplo2 class
    Commented out the entire Exemplo2 class and its methods.

--- Metodos/Exemplo2.java
+++ Metodos/Exemplo2.java
@@ -1,1 +1,27 @@
+/*
+import javax.swing.JOptionPane;
 
+public class Exemplo2 {
+
+    static void digite() {
+        JOptionPane.showMessageDialog(null, "Digite uma palavra:");
+    }
+
+    static int tamanho(String x) {
+        return x.length();
+    }
+
+    public static void main(String[] args) {
+
+        String p;
+        int t;
+
+        digite();
+        p = JOptionPane.showInputDialog("");
+        t = tamanho(p);
+
+        JOptionPane.showMessageDialog(null,
+                "Quantidade de caracteres: " + t);
+    }
+}
+*/

1910e2d7840908bf20e6d71198d0a9d1b01c61d5
Gustavo Lacerda <gustavo4kps4@gmail.com>
2026-03-04 13:17:49 -0300

    Atualizar o Hipotenusa.java

--- Metodos/Hipotenusa.java
+++ Metodos/Hipotenusa.java
@@ -1,21 +1,9 @@
-package com.mycompany.exemplo3;
+package com.mycompany.exercicio2;
 
-import javax.swing.JOptionPane;
+public class Hipotenusa {
 
-public class Exemplo3 {
-
-    public static void main(String[] args) {
-        double base, altura, h;
-
-        base = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da base: "));
-        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da altura: "));
-
-        h = hipotenusa(base, altura);
-
-        JOptionPane.showMessageDialog(null, "A hipotenusa do triângulo é: " + h);
+    public double hipotenusa(double base, double altura) {
+        return Math.sqrt((base * base) + (altura * altura));
     }
 
-    static double hipotenusa(double b, double a) {
-        return Math.sqrt((b * b) + (a * a));
-    }
 }

3208ad7a133e06c290fab13b9d4fe07c4a05bf44
Gustavo Lacerda <gustavo4kps4@gmail.com>
2026-03-04 13:19:37 -0300

    Update Pessoaex.java

--- Metodos/Pessoaex.java
+++ Metodos/Pessoaex.java
@@ -1,27 +1,4 @@
 
-package com.mycompany.pessoaex;
-class Pessoa {
-    String nome;
-    String sexo;
-    String país; 
-
-    // Métodos definidos dentro da classe Pessoa
-    void anda() {
-        System.out.println(nome  + " , do sexo " + sexo + ", nascido no "+ país + ", esta andando. " );
-    }
-    void fala() {
-        System.out.println(nome  + " , do sexo " + sexo + ", nascido no "+ país + ", esta falando.");
-    }
-    void corre() {
-        System.out.println(nome  + " , do sexo " + sexo + ", nascido no "+ país + ", esta correndo.");
-    }
-    void estuda() {
-        System.out.println(nome  + " , do sexo " + sexo + ", nascido no "+ país + ", esta estudando. ");
-    }
-    void brinca() {
-        System.out.println(nome  + " , do sexo " + sexo + ", nascido no "+ país + ", esta brincando.");
-    }
-}
 public class Pessoaex {
 
     public static void main(String[] args) {

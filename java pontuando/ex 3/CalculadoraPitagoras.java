cd118ed99c59c470ab191bab9a4ef8899302698e
Gustavo Lacerda <gustavo4kps4@gmail.com>
2026-03-12 11:40:19 -0300

    Create CalculadoraPitagoras.java

---
+++ java pontuando/ex 3/CalculadoraPitagoras.java
@@ -0,0 +1,8 @@
+package com.mycompany.pitagoras;
+
+public class CalculadoraPitagoras {
+
+    double calcularHipotenusa(double a, double b) {
+        return Math.sqrt((a * a) + (b * b));
+    }
+}
